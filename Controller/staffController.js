b8d85c404ffdd7b9dfcfa662ead25a4dbf41c615
thisaru <amithawerahena@gmail.com>
2024-12-02 20:05:46 +0530

    setup staff member saved,update,remove

--- Controller/staffController.js
+++ Controller/staffController.js
@@ -1,4 +1,11 @@
-import {saveStaffMember,generateNewStaffId , getAllStaffMembers} from "../ControllerModel/staffModel.js"
+import {
+    saveStaffMember,
+    generateNewStaffId,
+    getAllStaffMembers,
+    getStaffMember ,
+    updateStaffMember,
+    deleteStaffMember
+} from "../ControllerModel/staffModel.js"
 
 $(document).ready(function(){
     
@@ -16,11 +23,106 @@ $('#saveStaffBtn').click( function(){
     
     saveStaffMember(getStaffObject()).then(()=>{
         alert("Staff Member Added Successfully");
+        updateTableWithSaving(getStaffObject())
         resetForms();
+        
     }).catch((error) => { 
         console.error("Error:", error);
     });
 
+});
+
+$('#updateStaffBtn').click( function (){
+    const member = getStaffObject();
+    updateStaffMember(member).then(()=>{
+        alert("Staff Member Added Successfully");
+        updateTableWithUpdateProcess(member)
+        resetForms();
+        $("#staffModal").modal("hide");
+    }).catch((error) => { 
+        console.error("Error:", error);
+    });
+});
+
+$('#removeStaffBtn').click( function (){
+    let deleteId = selectedRow.find("#sId").text() 
+    deleteStaffMember(deleteId).then(()=>{
+        alert("Staff Member Added Successfully");
+        selectedRow.remove()
+        resetForms();
+        $("#staffModal").modal("hide");
+    }).catch((error) => { 
+        console.error("Error:", error);
+    });
+});
+
+function updateTableWithUpdateProcess(member){
+    selectedRow.find("td:nth-child(1)").text(member.id);
+    selectedRow.find("td:nth-child(2)").text(member.firstName);
+    selectedRow.find("td:nth-child(3)").text(member.designation);
+    selectedRow.find("td:nth-child(4)").text(member.gender);
+    selectedRow.find("td:nth-child(5)").text(member.joinedDate);
+    selectedRow.find("td:nth-child(6)").text(member.contactNo+"\n"+member.email);
+}
+
+function updateTableWithSaving(obj){
+    $("#staffTableBody").append(
+        `<tr class="cropDetails">
+            <td id = "sId">${obj.id}</td>
+            <td>${obj.firstName}</td>
+            <td>${obj.designation}</td>
+            <td>${obj.gender}</td>
+            <td>${obj.dateOfBirth}</td>
+            <td>${obj.contactNo}<br>${obj.email}</td>
+            <td> <button class="btn btn-success view-btn">Edit</button></td>
+        </tr>`
+    );
+}
+
+
+let selectedRow = null;
+$(document).on("click", ".view-btn", function () {
+    
+    
+    selectedRow = $(this).closest("tr"); 
+    $("#staffModal").modal("show");
+    $("#saveStaffBtn").hide()
+    $("#removeStaffBtn").show()
+    $("#updateStaffBtn").show()
+
+    const id = selectedRow.find("#sId").text();  
+    getStaffMember(id,(member)=>{
+        $('#staffId').val(member.id)
+        $('#firstName').val(member.firstName),
+        $('#lastName').val(member.lastName),
+        $('#designation').val(member.designation),
+        $('#dob').val(member.dateOfBirth),
+        $('#gender').val(member.gender),
+        $('#joinedDate').val(member.joinedDate),
+        $('#addressLine1').val(member.addressLine1),
+        $('#addressLine2').val(member.addressLine2),
+        $('#addressLine3').val(member.addressLine3),
+        $('#addressLine4').val(member.addressLine4),
+        $('#addressLine5').val(member.addressLine5),
+        $('#contact').val(member.contactNo),
+        $('#email').val(member.email),
+        $('#role').val(member.role)
+    });
+    
+});
+
+
+
+//chooseDesignation
+
+
+$(document).on("click", "#addMember", function () {
+    
+    $("#staffModal").modal("show");
+    $("#saveStaffBtn").show()
+    $("#removeStaffBtn").hide()
+    $("#updateStaffBtn").hide()
+    
 });
 
 
--- ControllerModel/staffModel.js
+++ ControllerModel/staffModel.js
@@ -57,7 +57,7 @@ export function getAllStaffMembers(){
                   <td>${member.firstName}</td>
                   <td>${member.designation}</td>
                   <td>${member.gender}</td>
-                  <td>${member.dateOfBirth}</td>
+                  <td>${member.joinedDate}</td>
                   <td>${member.contactNo}<br>${member.email}</td>
                   <td> <button class="btn btn-success view-btn">Edit</button></td>
               </tr>`
@@ -75,5 +75,73 @@ export function getAllStaffMembers(){
   http.send();
 }
 
+export function getStaffMember(id,setVslues){
+  return new Promise((resolve,reject)=>{
+    const http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+      if (http.readyState === 4) {
+        if (http.status === 200) {
+            const member = JSON.parse(http.responseText)
+            console.log(member)
+            setVslues(member)
+            resolve();
+        } else {
+          console.error("Failed");
+          console.error("Status", http.status);
+          console.error("Ready State", http.readyState);
+          reject(new Error("Failed to update crop with status: " + http.status));
+        }
+      }
+    };
+
+    http.open("GET", "http://localhost:8080/greenShadow/api/v1/staff/"+id, true);
+    http.setRequestHeader("Content-Type","application/json");
+    http.send();
+  });
+}
+
+export function updateStaffMember(staffObj){
+    console.log(staffObj)
+    const staffObjJSON = JSON.stringify(staffObj)
+    return new Promise((resolve,reject)=>{
+        const http = new XMLHttpRequest();
+        http.onreadystatechange = () => {
+          if (http.readyState === 4) {
+            if (http.status === 204) {
+                resolve();
+            } else {
+              console.error("Failed");
+              console.error("Status", http.status);
+              console.error("Ready State", http.readyState);
+              reject(new Error("Failed to update Staff Member with status: " + http.status));
+            }
+          }
+        };
+    
+        http.open("PATCH", "http://localhost:8080/greenShadow/api/v1/staff/"+staffObj.id, true);
+        http.setRequestHeader("Content-Type","application/json");
+        http.send(staffObjJSON);
+    });
+}
 
 
+export function deleteStaffMember(id){
+  console.log(id)
+  return new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+      if (http.readyState === 4) {
+        if (http.status === 204) {
+          resolve(); // Resolve the promise on success
+        } else {
+          console.error("Failed");
+          console.error("Status", http.status);
+          console.error("Ready State", http.readyState);
+          reject(new Error("Failed to delete Staff member with status: " + http.status)); // Reject the promise on failure
+        }
+      }
+    };
+    http.open("DELETE", "http://localhost:8080/greenShadow/api/v1/staff/"+id, true);
+    http.send();
+  });
+} 
